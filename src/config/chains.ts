dfa49ff91bd32d762e45b98f2040caa0bc551e1e
Sumbat <sumbat15@gmail.com>
2025-03-10 14:36:39 +0200

    fix: removed chain from sdk init

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -5,18 +5,19 @@ export const chains = {
 } as const
 
 export const chainIds = [holesky.id] as const
+export type ChainId = (typeof chainIds)[number]
 export const networks = ['holesky'] as const
 export type Network = Lowercase<(typeof networks)[number]>
-export const bam_graph_endpoints: Record<Network, string> = {
-  holesky:
+export const bam_graph_endpoints: Record<ChainId, string> = {
+  [holesky.id]:
     'https://api.studio.thegraph.com/query/71118/based-applications-ssv-holesky/version/latest/',
 }
 
 export type ContractAddresses = {
   bapp: Address
 }
-export const contracts: Record<Network, ContractAddresses> = {
-  holesky: {
+export const contracts: Record<ChainId, ContractAddresses> = {
+  [holesky.id]: {
     bapp: '0x352A18AEe90cdcd825d1E37d9939dCA86C00e281',
   },
 }
