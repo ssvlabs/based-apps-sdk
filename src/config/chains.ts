e3b12d3329318ef54670891c0cdfc1fba7dc712f
sumbat-ssvlabs <sumbat@ssvlabs.io>
2025-03-31 15:55:36 +0300

    Feature/milestone 5 (#6)
    * feat: added hoodi chain
    
    * fix: added hoodi subgraph endpoint
    
    * chore: bumped version

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,16 +1,37 @@
 import type { Address } from 'abitype'
+import { defineChain } from 'viem'
 import { holesky } from 'viem/chains'
+
+export const hoodi = defineChain({
+  id: 560048,
+  name: 'Hoodi',
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.hoodi.ethpandaops.io'],
+    },
+  },
+  nativeCurrency: {
+    name: 'Hoodi Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  testnet: true,
+})
+
 export const chains = {
   holesky,
+  hoodi,
 } as const
 
-export const chainIds = [holesky.id] as const
+export const chainIds = [holesky.id, hoodi.id] as const
 export type ChainId = (typeof chainIds)[number]
-export const networks = ['holesky'] as const
+export const networks = ['holesky', 'hoodi'] as const
 export type Network = Lowercase<(typeof networks)[number]>
 export const bam_graph_endpoints: Record<ChainId, string> = {
   [holesky.id]:
     'https://api.studio.thegraph.com/query/71118/based-applications-ssv-holesky/version/latest/',
+  [hoodi.id]:
+    'https://graph-node-hoodi.stage.ops.ssvlabsinternal.com/subgraphs/name/ssv-bapps-hoodi-stage/graphql',
 }
 
 export type ContractAddresses = {
@@ -19,5 +40,8 @@ export type ContractAddresses = {
 export const contracts: Record<ChainId, ContractAddresses> = {
   [holesky.id]: {
     bapp: '0x9B3345F3B1Ce2d8655FC4B6e2ed39322d52aA317',
+  },
+  [hoodi.id]: {
+    bapp: '0x3F2983b813054Eba76Ae137DfA77836CA8b00ACE',
   },
 }
