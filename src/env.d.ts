83a46f7ea3c8542020a334bd8bf999a9d797c13c
sumbat-ssvlabs <sumbat@ssvlabs.io>
2025-07-15 15:04:49 +0300

    fix: updated packages

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,4 +7,20 @@ interface ImportMetaEnv {
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
-} 
+}
+
+declare module 'ssv-keys' {
+  export interface KeySharesItem {
+    data: {
+      publicKey: string
+      operators: Array<{
+        id: number
+        operatorKey: string
+      }>
+    }
+    payload: {
+      operatorIds: number[]
+    }
+    error: Error | null
+  }
+}
